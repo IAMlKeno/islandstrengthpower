7481e2805b8379e790d552b6552d5bbd3e7e2343
Elkeno Jones <elkeno.jones@binarystar.ca>
2024-12-03 12:48:28 -0400

    init drupal

---
+++ drupal/custom/themes/island_strength_power_theme/js/island-strength-power-theme.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * Island Strength and Power Theme behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.islandStrengthPowerTheme = {
+    attach (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
