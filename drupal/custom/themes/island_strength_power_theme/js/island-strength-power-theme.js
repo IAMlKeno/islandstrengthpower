8706b70f6079f351d5353c2f192803052c2845c2
Elkeno Jones <elkeno.jones@binarystar.ca>
2024-12-10 21:42:06 -0400

    feat: create event forms for strongman and powerlifting and make available to events

--- drupal/custom/themes/island_strength_power_theme/js/island-strength-power-theme.js
+++ drupal/custom/themes/island_strength_power_theme/js/island-strength-power-theme.js
@@ -9,7 +9,6 @@
   Drupal.behaviors.islandStrengthPowerTheme = {
     attach (context, settings) {
 
-      console.log('It works!');
       const registrationForm = document.getElementsByClassName('isp-registration-form').item(0);
 
       if (registrationForm != null) {
@@ -19,16 +18,6 @@
           registrationForm.classList.add('hidden');
         }
       }
-
-      const paymentContainer = document.getElementsByClassName('payment-container').item(0);
-      if (paymentContainer != null) {
-        // const html = '<h2>Payment-Container</h2>';
-        // const iframe = document.createElement('iframe');
-        // // iframe.src = 'data:text/html;charset=utf-8,' + encodeURI(html);
-        // iframe.src = `/simple-square-plugin/req-payment/45/ISPEvent`;
-        // paymentContainer.appendChild(iframe);
-      }
-
     }
   };
 
