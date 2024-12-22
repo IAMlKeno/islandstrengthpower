b16099a566a46e407b4ce9e4d1f7ea655d45ad2b
Elkeno Jones <elkeno.jones@binarystar.ca>
2024-12-21 23:03:00 -0400

    feat: simple jquery ro hide and show form elements

--- drupal/custom/themes/island_strength_power_theme/js/island-strength-power-theme.js
+++ drupal/custom/themes/island_strength_power_theme/js/island-strength-power-theme.js
@@ -8,45 +8,22 @@
 
   Drupal.behaviors.islandStrengthPowerTheme = {
     attach (context, settings) {
-    //   const {
-    //     publishableKey, clientSecret, webformId, clientEmail,
-    //     clientName, collectAddress, addressType, appearanceEnabled, paymentAppearance,
-    // } = drupalSettings.stripeWebformPayment;
-
-
       const registrationForm = document.getElementsByClassName('isp-registration-form').item(0);
+      const btn = $('#registration-btn');
 
       if (registrationForm != null) {
-        console.log('It works!');
         const path = window.location.pathname;
         if (path.match(/\/event\/\w+/) != null) {
-          // registrationForm.classList.add('hidden');
+          registrationForm.classList.add('hidden');
+
+          btn.click((e) => {
+            $( registrationForm ).slideDown(600);
+            registrationForm.classList.remove('hidden');
+            $( e.target ).fadeOut();
+          });
         }
       }
 
-
-
-      // const btn = document.getElementById('registration-btn');
-      const btn = $('#registration-btn');
-      // const val = document.getElementsByName('registration-val')[0].value;
-      const nid = 4;
-      // const title = "New Powerlifting event";
-      // const form = document.getElementsByClassName('isp-registration-form').item(0);
-
-      // btn.addEventListener('click', (e) => {
-      btn.click((e) => {
-        // const stripe = Stripe(publishableKey);
-        // const paymentIntent = stripe.elements({});
-        // console.log(`${paymentIntent}`);
-        // console.log(`evt value: ${val}`);
-        // console.log(`nid value: ${nid}`);
-        // console.log(`title value: ${title}`);
-
-        // if (form != null) {
-        //   form.classList.remove('hidden');
-        //   e.target.classList.add('hidden');
-        // }
-      });
     }
   };
 
